c063b0e8c466bec36b583a6d62dfaf17e552b7d3
gitaud <denisgitau12@gmail.com>
2022-12-28 20:38:05 +0300

    Convert manual slider into carousel

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,77 +1,71 @@
 import React from 'react';
-import { useState } from 'react';
 import styled from 'styled-components';
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material';
+import Carousel from 'react-material-ui-carousel';
+
 import { sliderItems } from '../data';
 import { mobile } from "../responsive";
 
-const Container = styled.div`
-	max-width: 100vw;
-	height: 100vh;
-	display: flex;
-	position: relative; 
-	overflow: hidden;
-	${mobile({display: "none"})}
-`
-
-const Arrow = styled.div`
-	width: 50px;
-	height: 50px;
-	background-color: #fff7f7;
-	border-radius: 50%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: ${props => props.direction === "left" && "10px"};
-	right: ${props => props.direction === "right" && "10px"};
-	margin: auto;
-	cursor: pointer;
-	opacity: 0.5;
-	z-index: 2;
-`
-
-const Wrapper = styled.div`
-	height: 100%;
-	display: flex;
-	flex-direction: row;
-	transition: all 1.5s ease;
-	transform: translateX(${props => props.slideIndex * -100}vw);
-`
 
 const Slide = styled.div`
 	display: flex;
 	align-items: center;
 	width: 100vw;
-	height: 100vh;
+	height: 500px;
 	background-color: #${props => props.bg}
 `
 const ImgContainer = styled.div`
 	flex: 1;
-	height: 100%;
+	height: 95%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 
+	${mobile({
+		display: "none"
+	})}
+
 `
 const InfoContainer = styled.div`
 	flex: 1;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	@media only screen and (max-width: 450px) {
+		background-color: none;
+		background-image: url(${props => props.img});
+		background-size: cover;
+	}
 `
 
 const Image = styled.img`
-	height: 80%;
+	height: 100%;
 `
 const Title = styled.h1`
-	font-size: 70px
+	font-size: 70px;
+
+	${mobile({
+		color: "#ffffff",
+		textAlign: "center",
+		fontSize: "45px"
+	})}
 `
 
 const Desc = styled.p `
-	margin: 50px 0px;
+	margin: 50px 10px;
 	font-size: 20px;
 	font-weight: 500;
 	letter-spacing: 3px;
+
+	${mobile({
+		textAlign: "center",
+		color: "#ffffff",
+		margin: "30px 0px",
+		letterSpacing: "2px"
+	})}
 `
 
 
@@ -79,41 +73,39 @@ const Button = styled.button`
 	padding: 10px;
 	font-size: 20px;
 	background-color: transparent;
-	cursor: pointer
+	cursor: pointer;
+
+	${mobile({
+		backgroundColor: "#ffffff",
+		color: "#000000",
+	})}
 `
 
 const Slider = () => {
-	const [slideIndex, setSlideIndex] = useState(0);
-	const handleClick = (direction) => {
-		if (direction === "left") {
-			setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2);
-		} else {
-			setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
-		}
-	}
 	return (
-		<Container>
-				<Arrow direction="left" onClick={ () => handleClick("left")}>
-					<ArrowLeftOutlined />
-				</Arrow>
-				<Wrapper slideIndex={slideIndex}>
-					{sliderItems.map(item => (
-						<Slide bg={item.bg} key={item.id}>
-						<ImgContainer>
-							<Image src={item.img}/>
-						</ImgContainer>
-						<InfoContainer>
-							<Title>{item.title}</Title>
-							<Desc>{item.desc}</Desc>
-							<Button onClick={() => window.location="#contactForm"}>Contact Us</Button>
-						</InfoContainer>
-					</Slide>
-					))}
-				</Wrapper>
-				<Arrow direction="right" onClick={ () => handleClick("right")}>
-					<ArrowRightOutlined />
-				</Arrow>
-		</Container>
+		<Carousel
+			stopAutoPlayOnHover={false}
+			interval={5000}
+			duration={700}
+			navButtonsAlwaysVisible={true}
+			indicatorButtonProps={
+				{'position': 'absolute','bottom': '10px', 'margin': '0px'}
+
+			}
+		>
+			{sliderItems.map(item => (
+				<Slide bg={item.bg} key={item.id}>
+					<ImgContainer>
+						<Image src={item.img}/>
+					</ImgContainer>
+					<InfoContainer img={item.img}>
+						<Title>{item.title}</Title>
+						<Desc>{item.desc}</Desc>
+						<Button onClick={() => window.location="#contactForm"}>Contact Us</Button>
+					</InfoContainer>
+				</Slide>
+			))}
+		</Carousel>
 	)
 }
 
