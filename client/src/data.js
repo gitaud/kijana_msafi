36cd6f879025b83a4f12c1c07ef9b4781b785688
gitaud <denisgitau12@gmail.com>
2022-12-28 20:39:22 +0300

    Modify slider item descriptions

--- client/src/data.js
+++ client/src/data.js
@@ -3,21 +3,21 @@ export const sliderItems = [
 		id: 1,
 		img: "https://res.cloudinary.com/dctw6ghne/image/upload/v1671541605/catering_setup_xwczwo.jpg",
 		title: "Catering Services",
-		desc: "Amazing catering services for your event",
+		desc: "We offer services for corporate events, graduations, weddings, birthday parties and family gatherings",
 		bg: "f5fafd"
 	},
 	{
 		id: 2,
 		img: "https://res.cloudinary.com/dctw6ghne/image/upload/v1671543020/meat_on_a_grill_cooking_y1fj56.png",
 		title: "Outdoor Events",
-		desc: "Our team is well-trained and highly qualified",
+		desc: "Our team is well-trained and highly qualified. You'll enjoy top-tier services and great food",
 		bg: "fcf1ed"
 	},
 	{
 		id: 3,
 		img: "https://res.cloudinary.com/dctw6ghne/image/upload/v1671541623/cutlery_cf1rqf.jpg",
 		title: "Amazing food selection",
-		desc: "We'll sort all your needs under one roof",
+		desc: "All your needs under one roof. Contact us for bookings",
 		bg: "f5fafd"
 	}
 ]
