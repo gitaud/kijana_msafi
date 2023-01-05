641b3fd74bed4e279f8897e6915e7d3043cf59f7
gitaud <denisgitau12@gmail.com>
2023-01-05 10:49:44 +0300

    Add banner to profile

---
+++ client/src/components/BannerImg.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Img = styled.img`
+  width: 100%;
+`
+
+const Banner = () => {
+  return(
+    <Img src="https://res.cloudinary.com/dctw6ghne/image/upload/v1672643733/kijana_msafi_food_selection_ovsfwk.jpg" alt="Diverse selection of food offered by Kijana Msafi"/>  
+  )
+}
+
+export default Banner;
