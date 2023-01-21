ed9e33bce3b406b906efb5fc10330b88b5960d71
gitaud <denisgitau12@gmail.com>
2023-01-21 14:59:36 +0300

    Add logout function

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { logoutUser } from '../../redux/userRedux';
+import { logoutOrder } from '../../redux/orderRedux';
+import { useDispatch } from 'react-redux';
 import { 
 	PermIdentity,
 	Storefront,
+	PowerSettingsNew
 } from '@mui/icons-material';
 import "./Topbar.css";
 
 export default function Topbar() {
+
+	const dispatch = useDispatch();
+
+	const logout = () => {
+		dispatch(logoutOrder());
+		dispatch(logoutUser());
+	}
+
 	return(
 		<div className='topbar'>
 			<div className='topbarWrapper'>
@@ -28,6 +40,9 @@ export default function Topbar() {
 					</div>
 					<Link to="/" className="link">
 						<img src="https://res.cloudinary.com/dctw6ghne/image/upload/v1671541603/kijana_msafi_logo_m1rdia.jpg" alt="Kijana Msafi Logo" className="topAvatar" />
+					</Link>
+					<Link onClick={logout} className="link">
+						<span className="logout"><PowerSettingsNew /></span>
 					</Link>
 				</div>
 			</div>
